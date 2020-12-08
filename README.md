82fb61822039e20b7c886d9bcd6aa57cd9dd02f7
Deanna Norman <deannaleenorman@msn.com>
2020-12-08 08:43:23 -0800

    adds jsonwebtoken package adds readme with installation instructions

---
+++ README.md
@@ -0,0 +1,10 @@
+# Exercise App
+
+##  Installation Instructions
+-  `npm i`
+- create a `.env` file in the root of the project
+- add the following to the `.env` file: 
+```
+DATABASE_URL=<database url goes here>/project3?retryWrites=true&w=majority
+SECRET=<secret string goes here>
+```
--- models/user.js
+++ models/user.js
@@ -30,10 +30,10 @@ userSchema.pre('save', function(next){
   if (!user.isModified('password')) return next();
   // password has been changed - salt and hash it
   bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
-  if (err) return next(err);
-  // replace the user provided password with the hash
-  user.password = hash;
-  next();
+    if (err) return next(err);
+    // replace the user provided password with the hash
+    user.password = hash;
+    next();
   });
 });
 
